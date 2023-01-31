64573adf5ac6d435e25015dd793787226703a888
hyju12 <113574806+hyju12@users.noreply.github.com>
2023-01-31 11:35:33 +0900

    [level 0] Title: 짝수의 합, Time: 0.04 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0 ; 
+        for(int a= 0; a<=n; a= a+2){
+            answer += a ;
+        }
+        
+        return answer;
+    }
+}
